993415f1533b664884d3df60db3c5a0d06c4f81d
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-04-27 09:06:32 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-27042021
     Your branch is up to date with 'origin/taglio-27042021'.
    
     Changes to be committed:
    	modified:   setup_node
    	modified:   src/root/Bin/ocsp-server.sh

--- src/root/Bin/ocsp-server.sh
+++ src/root/Bin/ocsp-server.sh
@@ -1,1 +1,1 @@
-openssl ocsp -port 9080 -index /etc/ssl/ca/db/index -rsigner /etc/ssl/ca/root-ca.crt -rkey /etc/ssl/ca/private/root-ca-nokey.key -CA /etc/ssl/ca/root-ca.crt -text 1>&-
+openssl ocsp -port 9080 -index /etc/ssl/ca/db/index -rsigner /etc/ssl/ca/root-ca.crt -rkey /etc/ssl/ca/private/root-ca-nopwd.key -CA /etc/ssl/ca/root-ca.crt -text 1>&-
